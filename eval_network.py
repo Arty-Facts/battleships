9173d9268ae2b103ec32d3e8ba07b5bb01bec212
Arturas Aleksandrauskas <artal938@student.liu.se>
2019-06-05 08:02:00 +0200

    added more models

--- eval_network.py
+++ eval_network.py
@@ -4,7 +4,7 @@ from config import *
 
 if __name__ == "__main__":
     #main()
-    models = ["taghunt100k", "taghunt200k", "taghunt500k"]
+    models = ["taghunt300k_lern100k", "taghunt300k", "taghunt200k", "taghunt200k_2seek100k", "taghunt200k_2seek100k_4seek100k"]
     print("Benshmarking with", BENCHMARK, "games")
     for model in models:
         agent = load_model(model)
