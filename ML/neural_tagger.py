bcb11cee661784c65debb51d79fa90bdd044cd80
Arturas Aleksandrauskas <artal938@student.liu.se>
2019-06-05 08:03:23 +0200

    added heatmap to network

--- ML/neural_tagger.py
+++ ML/neural_tagger.py
@@ -1,5 +1,8 @@
 from ML.tagger import Tagger
 from ML.network import Network
+import matplotlib.pyplot as plt
+from config import *
+import numpy as np
 import torch
 
 class NeuralTagger(Tagger):
@@ -11,6 +14,21 @@ class NeuralTagger(Tagger):
         inp = torch.tensor(state.get(), dtype=torch.float)
         scores = self.model.forward(inp)
         #TODO: pick the best and valid 
+        if HEAT_MAP:
+            result = []
+            #create a matix with 9 coloms 
+            data = list(map(float, scores))
+            for i in range(len(data)//10):
+                begin = i*10
+                end = (i+1)*10
+                result.append(data[begin : end])
+            fig = plt.figure(figsize=(state.higth, state.width))
+            fig.add_subplot(1, 2, 1)
+            plt.imshow(np.array(result), cmap='hot', interpolation='nearest')
+            fig.add_subplot(1, 2, 2)
+            plt.imshow(np.array(state.get_map()) * 10, cmap='bwr', interpolation=None)
+            plt.savefig("heatmap/out")
+            plt.close(fig)
         res = []
         for c,s in enumerate(scores):
             res.append((c,s))
